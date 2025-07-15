688ef7da386a8a95a79fc5f35eef7b36a3d7d955
Vadzim Uyunnik <vuyu.dev@gmail.com>
2025-07-15 22:27:26 +0200

    Implement structure of site. Add main and article pages

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format);
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/terminal.css/dist/terminal.min.css": "css/terminal.min.css",
+    "src/assets": "assets"
+  });
+
+
+  eleventyConfig.addCollection("articles", api =>
+    api.getFilteredByGlob("src/articles/*.md")
+      .sort((a, b) => b.date - a.date)
+  );
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
